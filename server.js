dbfb0c0ba8383944af46955fae9b8e5780515574
Mijail Montero <mijailmonterob@gmail.com>
2018-01-20 22:03:26 -0500

    Finished the MongoDB, create, and read parts of the tutorial

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+MongoClient.connect('mongodb://mish:mish@ds263837.mlab.com:63837/balevich', (err, database) => {
+  if (err)
+    return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  db.collection('messages').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('messages').save(req.body, (err, result) => {
+    if (err)
+      return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
